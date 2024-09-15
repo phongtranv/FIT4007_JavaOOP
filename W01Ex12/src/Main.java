5d758b6745d60bf02ee9da58d5371e89b1cc30f5
phongtranv <tranvanphong08092005@gmail.com>
2024-09-15 16:10:50 +0700

    Add files via upload

---
+++ W01Ex12/src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+           int a = 1;
+           int b = 2;
+           int c = a + b;
+           System.out.printf("Sum of a and b in: " + c);
+        }
+    }
+}
